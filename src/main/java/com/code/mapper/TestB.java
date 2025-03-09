4df843f205a39cac428f2e143aa16cd14796c63d
gbl <gbl403687@gmail.com>
2025-03-09 21:05:48 +0800

    test_first_commit

---
+++ src/main/java/com/code/mapper/TestB.java
@@ -0,0 +1,11 @@
+package com.code.mapper;
+
+/**
+ * @author gbl.huang
+ * @date 2025/03/09 21:05
+ **/
+public class TestB {
+    public static void main(String[] args) {
+        // dev first commit
+    }
+}
